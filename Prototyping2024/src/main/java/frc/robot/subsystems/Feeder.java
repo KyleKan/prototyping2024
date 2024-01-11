e8cce7b2287cec9d94c3e37f0d2d9683dfef7914
Rheanne <rheanneb@vandenrobotics.com>
2024-01-10 16:43:24 -0800

    Feeder Commit

---
+++ Prototyping2024/src/main/java/frc/robot/subsystems/Feeder.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Feeder extends SubsystemBase {
+  private TalonFX FeederMotor;
+  public FeederEnumState mFeederEnumState;
+  /** Creates a new Feeder. */
+  public enum FeederEnumState{
+    S_BallEnters, S_BallLeaves
+  }
+  public void RunFeederState(){
+    switch (mFeederEnumState) {
+      case S_BallEnters:
+      BallEnters();
+        break;
+    
+      case S_BallLeaves:
+      BallLeaves();
+        break;
+    }
+  }
+
+  public void BallEnters(){
+    mFeederEnumState = FeederEnumState.S_BallEnters;
+  }
+  public void BallLeaves(){
+
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
